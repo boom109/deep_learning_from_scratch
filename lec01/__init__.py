adf05d243efc3a766e9e133fe4612f308b97fec2
boom109 <boom10099@gmail.com>
2017-11-15 03:01:42 +0900

    update lec01

--- lec01/__init__.py
+++ lec01/__init__.py
@@ -1,8 +1,67 @@
+## 1.3.1 arithmetic operation
 print(1 - 2)
 print(4 * 5)
 print(7 / 5)
 print(3 ** 2)
 
+
+## 1.3.2 Data type
 print(type(10))
 print(type(2.718))
 print(type("hello"))
+
+
+## 1.3.3 Variable
+x = 10
+print(x)
+
+x = 100
+print(x)
+
+y = 3.14
+print(x * y)
+print(type(x * y))
+
+
+## 1.3.4 List
+a = [1, 2, 3, 4, 5]
+print(a)
+print(len(a))
+print(a[0])
+print(a[4])
+a[4] = 99
+print(a)
+print(a[0:2])
+print(a[1:])
+print(a[:3])
+print(a[:-1])
+print(a[:-2])
+
+
+## 1.3.5 Dictionary
+me = {'height':180}
+print(me['height'])
+me['weight'] = 70
+print(me)
+
+
+## 1.3.6 bool
+hungry = True
+sleepy = False
+print(type(hungry))
+print(not hungry)
+print(hungry and sleepy)
+print(hungry or sleepy)
+
+
+## 1.3.7 if statement
+
+
+
+## 1.3.8 for statement
+for i in [1,2,3]:
+    print(i)
+
+
+
+
